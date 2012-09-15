d2359ee1725d485155ab1539fe4ca32d2a1197af
Dmitri Brengauz <dmitri@momus.net>
2012-09-15 17:07:00 -0600

    changed variables to constatns in F_to_C

--- One/F_to_C.c
+++ One/F_to_C.c
@@ -3,19 +3,19 @@
 /* print Fahrenheit-Celsius table
    for fahr = 0, 20, ..., 300 */
 
+#define LOWER  0     /* LOWER limit of temperature table */
+#define UPPER  300   /* UPPER limit */
+#define STEP   20    /* STEP size */
+
 main()
 {
   float fahr, celsius ;
-  int lower, upper, step ;
+  /* int LOWER, UPPER, STEP ; */
 
-  lower = 0 ;   /* lower limit of temperature table */
-  upper = 300 ; /* upper limit */
-  step = 20 ;   /* step size */
-
-  fahr = lower ;
-  while (fahr <= upper) {
+  fahr = LOWER ;
+  while (fahr <= UPPER) {
     celsius = 5.0 * (fahr-32.0) / 9.0 ;
     printf("%3.0f %6.3f\n" , fahr, celsius) ;
-    fahr = fahr + step;
+    fahr = fahr + STEP;
   }
 }
